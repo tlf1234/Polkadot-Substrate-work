f57263d9ba1a41836b838f736cdb71e2d4e6cd44
tlf1234 <582901605@qq.com>
2024-04-24 15:10:31 +0800

    less_3_new

--- Less_3/mao_pao_sort/main.rs
+++ Less_3/mao_pao_sort/main.rs
@@ -1,8 +1,28 @@
 
-pub fn bubble_sort(arr: &mut [i32]) {
-    let len = arr.len();
-    for i in 0..len.saturating_sub(1) {
-        for j in 0..(len - 1 - i) {
+// pub fn bubble_sort(arr: &mut [i32]) {
+//     let len = arr.len();
+//     for i in 0..len.saturating_sub(1) {
+//         for j in 0..(len - 1 - i) {
+//             if arr[j] > arr[j + 1] {
+//                 arr.swap(j, j + 1);
+//             }
+//         }
+//     }
+// }
+ 
+
+// fn main() {
+//     println!("Hello, world!");
+//     let mut nums = [2, 20, 10, 4, 55, 3, 5];
+//     bubble_sort(&mut nums);
+//     println!("after sorted : {:?}", nums);
+// }
+
+
+// 泛型函数
+fn bubble_sort<T: std::cmp::PartialOrd>(arr: &mut [T]) {
+    for i in 0..arr.len() - 1 {
+        for j in 0..arr.len() - 1 - i {
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j + 1);
             }
@@ -10,10 +30,12 @@ pub fn bubble_sort(arr: &mut [i32]) {
     }
 }
  
-
 fn main() {
-    println!("Hello, world!");
-    let mut nums = [2, 20, 10, 4, 55, 3, 5];
+    let mut nums = [40, 6, 8, 3, 2, 28];
     bubble_sort(&mut nums);
-    println!("after sorted : {:?}", nums);
+    println!("{:?}", nums); // 输出排序后的数组
+ 
+    let mut chars = ['c', 'd', 'a', 'f', 'g'];
+    bubble_sort(&mut chars);
+    println!("{:?}", chars); // 输出排序后的字符数组
 }
